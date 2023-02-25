dcb221f93510dc6d387359b40c2c2b6efc1a1ce0
pavelmacbook <pa2003sha@gmail.com>
2023-02-26 00:15:09 +0200

    Changed num per users and time

--- src/test/java/com/shpp/pzobenko/gatling/gatling/GatlingTest.java
+++ src/test/java/com/shpp/pzobenko/gatling/gatling/GatlingTest.java
@@ -13,16 +13,16 @@ public class GatlingTest extends Simulation {
   public GatlingTest() {
     this.setUp(
         Scenario.sendMessageScenario1.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
+            CoreDsl.constantUsersPerSec(50).during(10)
         ),
         Scenario.sendMessageScenario2.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
+            CoreDsl.constantUsersPerSec(50).during(10)
         ),
         Scenario.sendMessageScenario3.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
+            CoreDsl.constantUsersPerSec(50).during(10)
         ),
         Scenario.sendMessageScenario4.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
+            CoreDsl.constantUsersPerSec(50).during(10)
         )
     ).protocols(httpProtocol);
   }
