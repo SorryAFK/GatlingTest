886c2a4c52bb06232259b46cec4ddeeac90fa514
pavelmacbook <pa2003sha@gmail.com>
2023-02-26 10:34:18 +0200

    Changed num per users and time

--- src/test/java/com/shpp/pzobenko/gatling/gatling/GatlingTest.java
+++ src/test/java/com/shpp/pzobenko/gatling/gatling/GatlingTest.java
@@ -1,5 +1,7 @@
 package com.shpp.pzobenko.gatling.gatling;
 
+import static io.gatling.javaapi.core.CoreDsl.incrementConcurrentUsers;
+
 import io.gatling.javaapi.core.CoreDsl;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpDsl;
@@ -12,17 +14,30 @@ public class GatlingTest extends Simulation {
 
   public GatlingTest() {
     this.setUp(
-        Scenario.sendMessageScenario1.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
-        ),
-        Scenario.sendMessageScenario2.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
-        ),
-        Scenario.sendMessageScenario3.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
-        ),
-        Scenario.sendMessageScenario4.injectOpen(
-            CoreDsl.constantUsersPerSec(10).during(10)
+        Scenario.sendMessageScenario1.injectClosed(
+            incrementConcurrentUsers(1)
+                .times(10)
+                .eachLevelLasting(1)
+                .separatedByRampsLasting(1)
+                .startingFrom(1)),
+        Scenario.sendMessageScenario2.injectClosed(
+            incrementConcurrentUsers(1)
+                .times(10)
+                .eachLevelLasting(1)
+                .separatedByRampsLasting(1)
+                .startingFrom(1)),
+        Scenario.sendMessageScenario3.injectClosed(
+            incrementConcurrentUsers(1)
+                .times(10)
+                .eachLevelLasting(1)
+                .separatedByRampsLasting(1)
+                .startingFrom(1)),
+        Scenario.sendMessageScenario4.injectClosed(
+            incrementConcurrentUsers(1)
+                .times(10)
+                .eachLevelLasting(1)
+                .separatedByRampsLasting(1)
+                .startingFrom(1)
         )
     ).protocols(httpProtocol);
   }
